0f62109b5492984e653014bbda5ea223c7f30ed5
ronal <ronal@MRRECREATED-PC>
2026-02-07 23:01:59 -0500

    Gemini ssuggestion

--- obj/Debug/net10.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713a6139cd3e16aa42efde154e0667dfb354a0d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5836d29b3689f641d4de3945fd40576389e3398f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
